e03126da3330d781e7ac13264031579115bbb64f
Secret Punk <amanjit.gill1@gmail.com>
2019-05-24 16:49:38 +1000

    Complete most of styling and call API.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,47 @@
+/*jshint esversion: 6 */
+
+const API_KEY = '9c828a3d330b676e787d3a6dca7bad1b';
+
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#input').val());
+  });
+});
+
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
+  var name, artist, imageURL, listeners;
+
+  for (var i = 0; i < tracks.length; i++) {
+
+    trackURL = tracks[i].url;
+    name = tracks[i].name;
+    artist = tracks[i].artist;
+    imageURL = tracks[i].image[1]["#text"];
+    listeners = tracks[i].listeners;
+
+    var oneRow =
+      '<div class="one-result row">' +
+        '<div class="col-xs-1 play-button">' +
+          '<a href=' + trackURL + '><i class="fa fa-play-circle-o fa-2x"></i></a>' +
+        '</div>' +
+        '<div class="col-xs-4">' + name + '</div>' +
+        '<div class="col-xs-4">' + artist + '</div>' +
+        '<div class="col-xs-2">' +
+          '<img src=' + imageURL + '>' +
+        '</div>' +
+        '<div class="col-xs-1 listeners">' + Trackster.padListeners(listeners) + '</div>' +
+      '</div>';
+
+    $('#results').append(oneRow);
+  }
 };
 
 /*
@@ -14,4 +50,27 @@ Trackster.renderTracks = function(tracks) {
 */
 Trackster.searchTracksByTitle = function(title) {
 
+  $.ajax( {
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    datatype: 'jsonp',
+    success: function(data) {
+      console.log(data.results.trackmatches.track);
+      Trackster.renderTracks(data.results.trackmatches.track);
+    }
+  });
+};
+
+/*
+  Pads number of listeners so that it is 8 digits long, making it easier
+  to line up all the numbers in the Listeners column of the results. */
+Trackster.padListeners = function(numString) {
+
+  const requiredLength = 8; /* allows for up to 99999999 listeners */
+  var paddingAmount = requiredLength - numString.length;
+
+  for (var i = 0; i < paddingAmount; i++) {
+    numString = '0' + numString;
+  }
+
+  return numString;
 };
